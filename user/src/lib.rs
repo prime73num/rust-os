74ee9b1e780bb8a10cf1bb252b11235d18202938
PG12138 <tmd@tencent>
2022-11-25 10:19:58 +0800

    add os

--- user/src/lib.rs
+++ user/src/lib.rs
@@ -5,12 +5,11 @@
 #[macro_use]
 pub mod console;
 mod lang_items;
-pub mod syscall;
+mod syscall;
 
 #[no_mangle]
 #[link_section = ".text.entry"]
 pub extern "C" fn _start() -> ! {
-    clear_bss();
     exit(main());
     panic!("unreachable after sys_exit!");
 }
@@ -21,16 +20,6 @@ fn main() -> i32 {
     panic!("Cannot find main!");
 }
 
-fn clear_bss() {
-    extern "C" {
-        fn start_bss();
-        fn end_bss();
-    }
-    (start_bss as usize..end_bss as usize).for_each(|addr| unsafe {
-        (addr as *mut u8).write_volatile(0);
-    });
-}
-
 use syscall::*;
 
 pub fn write(fd: usize, buf: &[u8]) -> isize {
@@ -38,4 +27,10 @@ pub fn write(fd: usize, buf: &[u8]) -> isize {
 }
 pub fn exit(exit_code: i32) -> isize {
     sys_exit(exit_code)
+}
+pub fn yield_() -> isize {
+    sys_yield()
+}
+pub fn get_time() -> isize {
+    sys_get_time()
 }
